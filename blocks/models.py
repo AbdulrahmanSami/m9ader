28de5b8fd0b345b9da64eee1d7c9ab0d0095a57c
Abdulrahman Sami <abo55od@hotmail.com>
2016-10-22 14:55:39 +0300

    first

--- blocks/models.py
+++ blocks/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
-from taggit.managers import TaggableManager
+'''from taggit.managers import TaggableManager'''
 
 college_choices = (
     ('M', u'كلية الطب'),
